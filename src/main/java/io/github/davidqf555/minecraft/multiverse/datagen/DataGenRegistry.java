709269b3296fb7469fd868449a9206039a3cefe2
DavidQF555 <davidfeng2003@gmail.com>
2024-11-12 04:30:47 -0600

    modified datagen event

--- src/main/java/io/github/davidqf555/minecraft/multiverse/datagen/DataGenRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/multiverse/datagen/DataGenRegistry.java
@@ -21,11 +21,13 @@ public final class DataGenRegistry {
 
     @SubscribeEvent
     public static void onGatherData(GatherDataEvent event) {
-        DataGenerator gen = event.getGenerator();
-        Map<ResourceLocation, NoiseGeneratorSettings> noise = new HashMap<>();
-        NoiseSettingsRegistry.SETTINGS.forEach((loc, val) -> noise.put(loc, val.settings()));
-        gen.addProvider(new CodecDataProvider<>(gen, Registry.NOISE_GENERATOR_SETTINGS_REGISTRY, NoiseGeneratorSettings.DIRECT_CODEC, noise));
-        gen.addProvider(new CodecDataProvider<>(gen, Registry.DIMENSION_TYPE_REGISTRY, DimensionType.DIRECT_CODEC, DimensionTypeRegistry.TYPES));
+        if (event.includeServer()) {
+            DataGenerator gen = event.getGenerator();
+            Map<ResourceLocation, NoiseGeneratorSettings> noise = new HashMap<>();
+            NoiseSettingsRegistry.SETTINGS.forEach((loc, val) -> noise.put(loc, val.settings()));
+            gen.addProvider(new CodecDataProvider<>(gen, Registry.NOISE_GENERATOR_SETTINGS_REGISTRY, NoiseGeneratorSettings.DIRECT_CODEC, noise));
+            gen.addProvider(new CodecDataProvider<>(gen, Registry.DIMENSION_TYPE_REGISTRY, DimensionType.DIRECT_CODEC, DimensionTypeRegistry.TYPES));
+        }
     }
 
 }
